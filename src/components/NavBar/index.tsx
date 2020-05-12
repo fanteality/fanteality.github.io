1812bc0685ba106c2209e10f16d0ede1b647e0ed
fanteality <1454001183@qq.com>
2020-05-12 18:22:42 +0800

    判断导航条是否显示

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,11 +3,11 @@ import './index.scss';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 const NavBar = (props: RouteComponentProps) => {
     const [navList, setList] = useState<string[]>([]);
+    let hideBanner = window.sessionStorage.getItem('hideBanner');
     useEffect(() => {
         setList(['首页', 'HTML', 'CSS', 'React']);
     }, []);
-    let { pathname = '' } = props.history.location;
-    return pathname === '/' ? null : (
+    return hideBanner ? (
         <div className="blog_navbar">
             {navList.map((ele, index) => (
                 <div className="blog_navbar-item" key={index}>
@@ -15,6 +15,6 @@ const NavBar = (props: RouteComponentProps) => {
                 </div>
             ))}
         </div>
-    );
+    ) : null;
 };
 export default withRouter(NavBar) as any;
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Banner from 'components/Banner';
 export default () => {
     let hideBanner = window.sessionStorage.getItem('hideBanner');
-    return <div className="blog_home">
-        {!hideBanner && <Banner />}
-        
-        </div>;
+    return (
+        <div className="blog_home">
+            {!hideBanner && <Banner />}
+            这是首页
+        </div>
+    );
 };
