a8fd6c515aa4b3e1d369be254621cf6a9be51665
fanteality <1454001183@qq.com>
2020-05-31 16:15:34 +0800

    引入AOS动画包

--- src/components/MotionEle/index.tsx
+++ src/components/MotionEle/index.tsx
@@ -1,22 +1,32 @@
-import React, { ReactElement } from 'react';
-import { Motion, spring, presets } from 'react-motion';
+import React, { ReactElement, useEffect } from "react";
+import { Motion, spring, presets } from "react-motion";
+import AOS from "aos";
+import "aos/dist/aos.css";
 interface Iprop {
-    className?: string;
-    attrname: string;
-    startValue: number;
-    targetValue: number;
-    children: ReactElement | string;
+  className?: string;
+  attrname: string;
+  startValue: number;
+  targetValue: number;
+  children: ReactElement | string;
 }
 export default (props: Iprop) => {
-    const { className = '', children, attrname, startValue, targetValue } = props;
-
-    return (
-        <Motion defaultStyle={{ motion: startValue }} style={{ motion: spring(targetValue, presets.wobbly) }}>
-            {(value) => (
-                <div className={className} style={{ transform: `${attrname}(${value.motion})` }}>
-                    {children}
-                </div>
-            )}
-        </Motion>
-    );
+  const { className = "", children, attrname, startValue, targetValue } = props;
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <Motion
+      defaultStyle={{ motion: startValue }}
+      style={{ motion: spring(targetValue, presets.wobbly) }}
+    >
+      {(value) => (
+        <div
+          className={className}
+          style={{ transform: `${attrname}(${value.motion})` }}
+        >
+          {children}
+        </div>
+      )}
+    </Motion>
+  );
 };
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
-import MotionEle from 'components/MotionEle';
-import DelayWrap from 'components/DelayWrap';
-import Toast from 'components/Toast';
-import { indexHot } from '../../utils/http';
-import logo from './img/logo.png';
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import MotionEle from "components/MotionEle";
+import DelayWrap from "components/DelayWrap";
+// import Toast from 'components/Toast';
+import { indexHot } from "../../utils/http";
+import logo from "./img/logo.png";
+import "./index.scss";
 
 const NavBar = (props: RouteComponentProps) => {
-    const [navList, setList] = useState<string[]>([]);
-    const hideBanner = window.sessionStorage.getItem('hideBanner');
-    useEffect(() => {
-        indexHot()
-            .then((res) => {
-                Toast.info("info",1000)
-            })
-            .catch((err) => {
-                console.log(2222, err);
-            });
-        setList(['首页', '前端', 'Java', '优站收藏', '杂七杂八']);
-    }, []);
-
-    return hideBanner ? (
-        <div className="blog_navbar">
-            <div className="blog_navbar_content">
-                <DelayWrap delay={1400}>
-                    <MotionEle className="blog_navbar_logo" attrname="scale" startValue={0} targetValue={1}>
-                        <img onClick={() => props.history.push('/')} src={logo} alt="" />
-                    </MotionEle>
-                </DelayWrap>
-                {navList.map((ele, index) => (
-                    <div className={'noselect blog_navbar_item_' + index} key={index}>
-                        {ele}
-                        <i className="hover_line"></i>
-                    </div>
-                ))}
-            </div>
-        </div>
-    ) : null;
+  const [navList, setList] = useState<string[]>([]);
+  const hideBanner = window.sessionStorage.getItem("hideBanner");
+  useEffect(() => {
+    indexHot()
+      .then((res) => {
+        // Toast.info("info",1000)
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(2222, err);
+      });
+    setList(["首页", "前端", "Java", "优站收藏", "杂七杂八"]);
+  }, []);
+  return hideBanner ? (
+    <div className="blog_navbar">
+      <div className="blog_navbar_content">
+        <MotionEle
+          className="blog_navbar_logo"
+          attrname="scale"
+          startValue={0}
+          targetValue={1}
+        >
+          <img onClick={() => props.history.push("/")} src={logo} alt="" />
+        </MotionEle>
+        {navList.map((ele, index) => (
+          <div className={"noselect blog_navbar_item_" + index} key={index}>
+            {ele}
+            <i className="hover_line"></i>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : null;
 };
 export default withRouter(NavBar) as any;
